bcec110133cf27adfeee8ec34dfa5b22766eda64
Salah Saleh <salahsaleh1015@gmail.com>
2025-11-04 12:40:07 +0200

    all expenses widget

--- lib/responsive_dashboard/dashboard_widgets/dashboard_desktop_layout.dart
+++ lib/responsive_dashboard/dashboard_widgets/dashboard_desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/dashboard_widgets/all_expenses.dart';
 import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/dashboard_widgets/dashboard_custom_drawer.dart';
 
 
@@ -8,10 +9,19 @@ class DashboardDesktopLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff7f9fa),
       body: Row(
         children: [
           Expanded(child: DashboardCustomDrawer()),
-        ],
+          SizedBox(width: 32),
+          Expanded(
+              flex: 2,
+              child: Column(
+            children: [
+              Expanded(child: AllExpenses()),
+            ],
+          ))
+          ],
       ),
     );
   }
