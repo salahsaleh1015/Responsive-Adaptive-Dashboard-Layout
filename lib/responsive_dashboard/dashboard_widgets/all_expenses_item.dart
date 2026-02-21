d1fc0f40606ce1c57a972e41d1b0d6f2ade81055
Salah Saleh <salahsaleh1015@gmail.com>
2026-02-21 13:48:21 +0200

    all expenses item data

--- lib/responsive_dashboard/dashboard_widgets/all_expenses_item.dart
+++ lib/responsive_dashboard/dashboard_widgets/all_expenses_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/dashboard_models/all_expenses_item_model.dart';
 import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/dashboard_widgets/all_expenses_item_header.dart';
 import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/utils/app_images.dart';
+import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/utils/app_styles.dart';
 
 class AllExpensesItem extends StatelessWidget {
   const AllExpensesItem({super.key, required this.allExpensesItemModel});
@@ -19,7 +20,24 @@ class AllExpensesItem extends StatelessWidget {
         color: Colors.white,
       ),
       child: Column(
-        children: [AllExpensesItemHeader(image: allExpensesItemModel.image)],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AllExpensesItemHeader(image: allExpensesItemModel.image),
+          Text(
+            allExpensesItemModel.title,
+            style: AppStyles.styleMedium16(context),
+          ),
+          SizedBox(height: 8,),
+          Text(
+            allExpensesItemModel.date,
+            style: AppStyles.styleRegular14(context),
+          ),
+          SizedBox(height: 16,),
+          Text(
+            allExpensesItemModel.price,
+            style: AppStyles.styleSemiBold24(context),
+          ),
+        ],
       ),
     );
   }
